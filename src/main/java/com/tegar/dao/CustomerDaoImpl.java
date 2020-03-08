fb6a4bdcd6c4cacd5f566d1b957133b7e3f1b18c
tegarjokam <dtegarjokam354@gmail.com>
2020-03-08 20:04:35 +0900

    Create Customer with test1_Create (Green)

--- src/main/java/com/tegar/dao/CustomerDaoImpl.java
+++ src/main/java/com/tegar/dao/CustomerDaoImpl.java
@@ -1,15 +1,22 @@
 package com.tegar.dao;
 
 import com.tegar.pojo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class CustomerDaoImpl implements CustomerDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public int create(Customer objectType) {
-        return 0;
+        return jdbcTemplate.update("INSERT INTO public.customer(nama, jenis_kelamin, alamat, created_time) VALUES (?, ?, ?, ?)",
+                objectType.getNama(), objectType.getJenisKelamin(), objectType.getAlamat(), objectType.getCreated_time());
     }
 
     @Override
